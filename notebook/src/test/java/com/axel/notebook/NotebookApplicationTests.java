b5a485bfcd12e0677e78a8b7953a0cf0896cdddf
Axel <135990741+axelrebollo@users.noreply.github.com>
2024-11-17 19:30:50 +0100

    Primer commit
    proyecto vacío + micro users + micro notebook

---
+++ notebook/src/test/java/com/axel/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axel.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
