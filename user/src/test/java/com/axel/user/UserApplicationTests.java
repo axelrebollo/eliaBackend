996e0b334cb8cda50aed5ca4c90073255cb7586b
Axel <axel.rebollo.cepero@gmail.com>
2025-02-14 18:02:47 +0100

    Actualizar configuraciones Notebook
    Se actualizan como en el microservicio users

--- user/src/test/java/com/axel/user/UserApplicationTests.java
+++ user/src/test/java/com/axel/user/UserApplicationTests.java
@@ -2,12 +2,8 @@ package com.axel.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
-})
 class UserApplicationTests {
 
     @Test
